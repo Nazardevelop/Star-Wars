ce0037f2340818be47d73935c90e047d924958b8
Nazar Tolmachov <tolmachovnazar@gmail.com>
2017-11-01 19:10:35 +0200

    comments
    added comments

--- js/script.js
+++ js/script.js
@@ -1,28 +1,18 @@
+/*this variable if for checking and editing checkboxes states.IT is global because it is easier to find mistakes and
+* project is small with one .js script, so we wil not have global name conflicts. If you do not want it to be global,
+ * replace it in window.onload, BUT then you will need to replace function changeState too
+* */
 var checkboxStates = {};
 window.onload = function(){
-    var checkBoxes = document.getElementsByClassName('checkbox');
-
-    var getJSON = function (url,callback) {
-    var  xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function () {
-        var status = xhr.status;
-        if (status === 200){
-           callback(null, xhr.response);
-        }
-        else {
-            callback(status, xhr.response);
-        }
-    };
-    xhr.send();
-};
+/*call function AJAX in window.onload, because we want our function to be executed when the page loads and to know
+ * what is persanagelist */
 getJSON("json/people.json",
     function(err, data) {
         //var p = document.getElementById('personagesList').innerHTML;
         if (err !== null) {
             alert('Something went wrong: ' + err);
         } else {
+            /*var p to create HTML code which will be injected to personagelist div*/
            var p = "<ul class='personages'>";
            var i = 1;
             data.results.forEach(function (elem) {
@@ -33,10 +23,13 @@ getJSON("json/people.json",
             p += "</ul>";
         }
         document.getElementById('personagesList').innerHTML = p;
+        /*here we get HTML collection of checkboxes*/
+        var checkBoxes = document.getElementsByClassName('checkbox');
         console.log(p);
         console.log(checkBoxes);
 
-
+/*if we have checkbox states data in session storage we write it in to checkboxStates variable, and give state from
+*that data to every checkbox */
         if(sessionStorage.getItem("checkBoxStates")){
             checkboxStates = JSON.parse(sessionStorage.getItem("checkBoxStates"));
             for (var key in checkboxStates){
@@ -44,6 +37,8 @@ getJSON("json/people.json",
             }
 
         }
+        /*if we have no info about checkbox states, we fill our checkboxStates object with it, where key is checkbox id
+        * and value is state. Then we put checkboxStates info to session storage*/
         else {
 
             for(var j = 0 ; j < checkBoxes.length; j++ ) {
@@ -55,6 +50,24 @@ getJSON("json/people.json",
     });
 
 };
+/* function for AJAX request*/
+ function getJSON (url,callback) {
+    var  xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        var status = xhr.status;
+        if (status === 200){
+            callback(null, xhr.response);
+        }
+        else {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
+};
+ /*function for listen the change of checkbox state. If the state is changed it will rewrite info about that checkbox in
+ * checkboxStates and in session storage*/
 function changeState(id) {
     for (var key in checkboxStates){
         if (key == id ){
